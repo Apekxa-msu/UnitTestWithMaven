d055cf6f340e48d172cc8dbc5f1bf74f54c32f40
Apekshya Thapa <athapa4@mcneese.edu>
2025-10-01 05:42:43 +0545

    Fix package to msu, update App.java and AppTest.java with JUnit tests

--- src/main/java/msu/App.java
+++ src/main/java/msu/App.java
@@ -1,18 +1,17 @@
 package msu;
 
 public class App {
-
     public int add(int a, int b) {
         return a + b;
     }
 
-    public int multiply(int a, int b) {
-        return a * b;
+    public String greet() {
+        return "Hello Jenkins";
     }
 
     public static void main(String[] args) {
         App app = new App();
-        System.out.println("2 + 3 = " + app.add(2, 3));
-        System.out.println("4 * 5 = " + app.multiply(4, 5));
+        System.out.println("Sum: " + app.add(2, 3));
+        System.out.println(app.greet());
     }
 }
--- src/test/java/msu/AppTest.java
+++ src/test/java/msu/AppTest.java
@@ -8,14 +8,12 @@ public class AppTest {
     @Test
     void testAddition() {
         App app = new App();
-        int result = app.add(2, 3);
-        assertEquals(5, result, "2 + 3 should be 5");
+        assertEquals(5, app.add(2, 3), "2 + 3 should equal 5");
     }
 
     @Test
-    void testMultiplication() {
+    void testGreeting() {
         App app = new App();
-        int result = app.multiply(4, 5);
-        assertEquals(20, result, "4 * 5 should be 20");
+        assertTrue(app.greet().contains("Jenkins"), "Greeting should contain Jenkins");
     }
 }
